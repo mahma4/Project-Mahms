aa4180688d7985fb7c9ae9615978fad34aad518b
mahma4 <156901529+mahma4@users.noreply.github.com>
2024-02-17 21:07:19 +0500

    Add files via upload

---
+++ project-mahma.py
@@ -0,0 +1,15 @@
+print("Microsoft Office is a suite of desktop productivity applications that is designed specifically by Microsoft for business use\nThree of these applications are:\n1. MS Word\n2. MS Power Point\n3. MS Excel ")
+print("MS Word is used to create, edit and format text based documents like letters, business proposals, reports etc")
+print("MS Power point is used to create presentations which can feature text, images, videos, art and much more. It is widely used in offices and classrooms.")
+print("MS Excel is used to format, organise and calculate data in a spreadsheet. It completes tasks like creating budgets, tracking expenses and making timetables.")
+print("Each application has its unique set of tools. However, there are a few MS Office tools that are common in the three applications. Following are a few examples:")
+print("Shapes-draw shapes on a file\nScreenshot-take screenshot of a screen being displayed\nChart-display series of numeric data in a graphical format")
+print("Smart Art-display text in different layouts including relationships and processes\nHyperlink-easily navigate to others sections in a file\nComment-feature suggestions, reminders or notes in a file")
+
+print("Temperature Converter: Celcius to Fahrenheit")
+a=int(input("Enter Temperature in °C ="))
+print(((a*1.8)+32), "°F")
+
+print("Temperature Converter: Fahrenheit to Celcius")
+b=int(input("Enter Temperature in °F ="))
+print(((b-32)*(5/9)), "°C")
